656431009a7cca4a076b2da734afcb616f37c771
Leonildo Filgueira <leonildolf_12@hotmail.com>
2023-08-28 20:20:46 -0300

    corrigindo método shipment

--- src/main/java/com/devsuperior/desafio_01/services/ShippingService.java
+++ src/main/java/com/devsuperior/desafio_01/services/ShippingService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 public class ShippingService {
 
     public double shipment(Order order) {
-        double shipmentTax = order.getBasic() < 100 ? 20.0 : order.getCode() <= 200 ? 12.0 : 0.0;
+        double shipmentTax = order.getBasic() < 100 ? 20.0 : order.getBasic() <= 200 ? 12.0 : 0.0;
         return shipmentTax;
     }
 }
